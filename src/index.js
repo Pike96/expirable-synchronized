397146787117c2f7acfaf02949c83b2fc8a8b686
Pike <pike.l@prodege.com>
2019-07-29 22:57:57 -0700

    Glitchy parameter setting and needs refactoring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
  * @type {string}
  */
 const LAST_PROMISE_PREFIX = 'expirable-synchronized-last-promise-';
-const PROMISE_LIFE = 10000;
+const DEFAULT_PROMISE_LIFE = 5000;
 
 /**
  * Block other function calls when one is in process by connecting all calls into a promise chain
@@ -11,7 +11,7 @@ const PROMISE_LIFE = 10000;
  *
  * @param PROMISE_LIFE: How long we wait for a function to return a promise
  */
-// export function expirableSynchronized(PROMISE_LIFE = 10000) {
+export function expirableSynchronized(...args) {
     /**
      * Inner decorator that takes function execution environment
      *
@@ -19,52 +19,77 @@ const PROMISE_LIFE = 10000;
      * @param funcName: Name of the function
      * @param descriptor: Details of the function
      */
-    export function expirableSynchronized(target, funcName, descriptor) {
-        console.log('target', target);
-        const clearLastPromise = () => {
-            // The pointer to build then-chain
-            target[ LAST_PROMISE_PREFIX + funcName ] = null;
-        };
-
-        const original = descriptor.value;
-        if (typeof original === 'function') {
-            descriptor.value = async function(...args) {
-                try {
-                    // Timeout promise
-                    let timeoutId;
-                    let timeout = new Promise((resolve, reject) => {
-                        timeoutId = setTimeout(() => {
-                            reject('Synchronized function timed out in ' + PROMISE_LIFE + 'ms.');
-                        }, PROMISE_LIFE);
-                    });
-
-                    // Function execution promise
-                    const applyDecoratee = () => original.apply(this, args);
-                    let promise;
-                    if (!target[ LAST_PROMISE_PREFIX + funcName ]) { // If pointer is empty, run it
-                        promise = applyDecoratee();
-                    }
-                    else { // Otherwise, connect the new one with the pointer by 'then'
-                        promise = target[ LAST_PROMISE_PREFIX + funcName ].then(applyDecoratee).catch(applyDecoratee);
-                    }
-
-                    // Race 2 promises. Only winner will be in the promise chain
-                    promise = Promise.race([
-                        promise,
-                        timeout
-                    ]).then(() => {clearTimeout(timeoutId);})
-                        .catch(() => {clearTimeout(timeoutId);});
-
-                    // Clear the pointer after done
-                    target[ LAST_PROMISE_PREFIX + funcName ] = promise.then(clearLastPromise).catch(clearLastPromise);
-                    return promise;
-                }
-                catch (e) {
-                    throw e;
-                }
-            };
-        }
-        return descriptor;
-    // };
+    return decorate(handleDescriptor, args);
 }
 
+function decorate(handleDescriptor, entryArgs) {
+    if (isDescriptor(entryArgs[entryArgs.length - 1])) {
+        return handleDescriptor(...entryArgs, []);
+    } else {
+        return function () {
+            return handleDescriptor(...Array.prototype.slice.call(arguments), entryArgs);
+        };
+    }
+}
+
+function isDescriptor(desc) {
+    if (!desc || !desc.hasOwnProperty) {
+        return false;
+    }
+    const keys = ['value', 'initializer', 'get', 'set'];
+
+    for (let i = 0, l = keys.length; i < l; i++) {
+        if (desc.hasOwnProperty(keys[i])) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function handleDescriptor(target, funcName, descriptor, [life = DEFAULT_PROMISE_LIFE]) {
+    const clearLastPromise = () => {
+        // The pointer to build then-chain
+        target[ LAST_PROMISE_PREFIX + funcName ] = null;
+    };
+
+    const original = descriptor.value;
+    if (typeof original === 'function') {
+        descriptor.value = async function(...args) {
+            try {
+                // Timeout promise
+                let timeoutId;
+                let timeout = new Promise((resolve, reject) => {
+                    timeoutId = setTimeout(() => {
+                        reject('Synchronized function timed out in ' + life + 'ms.');
+                    }, life);
+                });
+
+                // Function execution promise
+                const applyDecoratee = () => original.apply(this, args);
+                let promise;
+                if (!target[ LAST_PROMISE_PREFIX + funcName ]) { // If pointer is empty, run it
+                    promise = applyDecoratee();
+                }
+                else { // Otherwise, connect the new one with the pointer by 'then'
+                    promise = target[ LAST_PROMISE_PREFIX + funcName ].then(applyDecoratee).catch(applyDecoratee);
+                }
+
+                // Race 2 promises. Only winner will be in the promise chain
+                promise = Promise.race([
+                    promise,
+                    timeout
+                ]).then(() => {clearTimeout(timeoutId);})
+                    .catch(() => {clearTimeout(timeoutId);});
+
+                // Clear the pointer after done
+                target[ LAST_PROMISE_PREFIX + funcName ] = promise.then(clearLastPromise).catch(clearLastPromise);
+                return promise;
+            }
+            catch (e) {
+                throw e;
+            }
+        };
+    }
+    return descriptor;
+}
